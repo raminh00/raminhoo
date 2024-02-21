fe1c254e43fdbedaa50723a74343ebb7f9364f97
raminh00 <160696953+raminh00@users.noreply.github.com>
2024-02-21 07:48:23 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77c234bf-54a7-4cc2-9bef-462ee9b7ad0a';
+let userID = 'd56dc47e-8fae-423b-ac30-cea32475ffdf';
 let proxyIP = "49.51.46.56";
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
